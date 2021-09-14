0ad67c7ce5aeb0e6c8ba816a6864eaab78bbc743
loveyousomuch <loveyousomuch554@gmail.com>
2021-09-15 01:17:18 +0300

    download file if not exist

--- config/download_test_csv.sh
+++ config/download_test_csv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-CONFIGPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+CSV_FILE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/posts.csv
 
-curl -L "$CSV_FILE_URL" -o $CONFIGPATH/posts.csv
+if ! [ -f "$CSV_FILE" ]; then
+  curl -L "$CSV_FILE_URL" -o $CSV_FILE 
+fi
 
