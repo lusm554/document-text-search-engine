6d8b28b2d2cc6648fe4ebd716f32dd700d2dad9f
loveyousomuch <loveyousomuch554@gmail.com>
2021-09-15 04:41:50 +0300

    show status of csv dataset

--- config/download_test_csv.sh
+++ config/download_test_csv.sh
@@ -3,6 +3,7 @@
 CSV_FILE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/posts.csv
 
 if ! [ -f "$CSV_FILE" ]; then
+  echo "Downloading CSV dataset..."
   curl -L "$CSV_FILE_URL" -o $CSV_FILE 
 fi
 
