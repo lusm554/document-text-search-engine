a8c77a8bf1a4a0a509bf66fd1282a00cf155c446
loveyousomuch <loveyousomuch554@gmail.com>
2021-09-19 21:27:18 +0300

    fetch dotenv vars

---
+++ src/config.py
@@ -0,0 +1,11 @@
+from os import environ
+
+PG_USER = environ.get('PG_USER')
+PG_PWD = environ.get('PG_PWD')
+PG_HOSTNAME = environ.get('PG_HOSTNAME')
+PG_PORT = environ.get('PG_PORT')
+PG_DB = environ.get('PG_DB')
+ES_HOSTNAME = environ.get('ES_HOSTNAME')
+ES_PORT = environ.get('ES_PORT')
+ES_INDEX = environ.get('ES_INDEX')
+
